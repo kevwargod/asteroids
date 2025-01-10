1add3f9664f378954b329e3bb83ffa4f0a89e33c
kevwargod <kevares.social@gmail.com>
2025-01-11 03:37:38 +0800

    added player and shape modules

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player1 = Player(x,y)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
     print("Starting asteroids!")
@@ -14,7 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
 
 if __name__=="__main__":
