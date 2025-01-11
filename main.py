5806f599613030bc75d6c2d8a3fce3916efb3bf5
kevwargod <kevares.social@gmail.com>
2025-01-11 14:16:08 +0800

    added update and rotate methods to player class

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
+        player1.update(dt)
         player1.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
-        
 
 if __name__=="__main__":
     main()
--- player.py
+++ player.py
@@ -18,3 +18,14 @@ class Player(CircleShape):
     
     def draw(self, screen):
         circle = pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-1 * dt)
+        if keys[pygame.K_d]:
+            self.rotate(1 * dt)
